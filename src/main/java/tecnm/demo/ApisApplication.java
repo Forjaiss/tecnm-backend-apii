350e86818a37a7e2f5d9ec382894581a1a107934
Forjaiss <jorgeamezcua575@gmail.com>
2025-11-12 17:02:12 +0000

    Agregue todo lo de clase

--- src/main/java/tecnm/demo/ApisApplication.java
+++ src/main/java/tecnm/demo/ApisApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApisApplication.class, args);
+		SpringApplication.run(
+		ApisApplication.class, args);
 	}
 
 }
